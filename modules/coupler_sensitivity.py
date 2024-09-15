d149062c2916f3eb08adb7e7864e4a65a0d1e2b1
Pierce Zhang <143103585+isosafrasaurus@users.noreply.github.com>
2024-09-15 08:42:11 +0000

    New library

---
+++ modules/coupler_sensitivity.py
@@ -0,0 +1,206 @@
+import vtk
+import meshio
+import os
+import numpy as np
+import networkx as nx
+from dolfin import *
+from xii import *
+from graphnics import *
+from scipy.spatial import cKDTree
+
+def save_mesh_vtk(Lambda, file_path, radius_map_G, uh1d=None):
+    
+    points = Lambda.coordinates()
+    cells = {"line": Lambda.cells()}
+
+    
+    radius_values = np.array([radius_map_G(point) for point in points])
+
+    
+    if uh1d is not None:
+        uh1d_values = np.array([uh1d(point) for point in points])
+        mesh = meshio.Mesh(points, cells, point_data={"radius": radius_values, "Pressure1D": uh1d_values})
+    else:
+        mesh = meshio.Mesh(points, cells, point_data={"radius": radius_values})
+    mesh.write(file_path)
+
+    
+    reader = vtk.vtkUnstructuredGridReader()
+    reader.SetFileName(file_path)
+    reader.Update()
+
+    
+    geometryFilter = vtk.vtkGeometryFilter()
+    geometryFilter.SetInputData(reader.GetOutput())
+    geometryFilter.Update()
+
+    polydata = geometryFilter.GetOutput()
+
+    
+    writer = vtk.vtkPolyDataWriter()
+    writer.SetFileName(file_path)
+    writer.SetInputData(polydata)
+    writer.Write()
+
+class radius_function(UserExpression):
+    
+    def __init__(self, G, mf, kdtree, **kwargs):
+        self.G = G
+        self.mf = mf
+        self.kdtree = kdtree
+        super().__init__(**kwargs)
+
+    def eval(self, value, x):
+        p = (x[0], x[1], x[2])
+        _, nearest_control_point_index = self.kdtree.query(p)
+        nearest_control_point = list(self.G.nodes)[nearest_control_point_index]
+        value[0] = self.G.nodes[nearest_control_point]['radius']
+
+    def value_shape(self):
+        return ()
+
+def run_perfusion_univ(G, directory_path, del_Omega=3.0, perf3=9.6e-2, perf1=1.45e4, kappa_value=3.09e-5, gamma=1.0, P_infty=1.0e3):
+    
+    
+    G.make_mesh()
+    Lambda, mf = G.get_mesh()
+
+    
+    H = G.copy()
+
+    
+    Omega = UnitCubeMesh(16, 16, 16)
+
+    
+    pos = nx.get_node_attributes(G, "pos")
+    node_coords = np.asarray(list(pos.values()))
+    xmin, ymin, zmin = np.min(node_coords, axis=0)
+    d = Lambda.coordinates()
+    d[:, :] += [-xmin, -ymin, -zmin]
+    for node in H.nodes:
+        H.nodes[node]['pos'] = np.array(H.nodes[node]['pos']) + [-xmin, -ymin, -zmin]
+
+    
+    kdtree = cKDTree(np.array(list(nx.get_node_attributes(H, 'pos').values())))
+
+    
+    c = Omega.coordinates()
+    xl, yl, zl = (np.max(node_coords, axis=0) - np.min(node_coords, axis=0))
+    c[:, :] *= [xl + 3, yl + 3, zl]
+
+    def boundary_Omega(x, on_boundary):
+        return on_boundary and not near(x[2], 0) and not near(x[2], zl)
+
+    
+    kappa = Constant(kappa_value)
+    gamma = Constant(gamma)
+    P_infty = Constant(P_infty)
+    del_Omega = Constant(del_Omega)
+
+    
+    V3 = FunctionSpace(Omega, "CG", 1)
+    V1 = FunctionSpace(Lambda, "CG", 1)
+    W = [V3, V1]
+    u3, u1 = list(map(TrialFunction, W))
+    v3, v1 = list(map(TestFunction, W))
+
+    radius_map_G = radius_function(G, mf, kdtree)
+    cylinder = Circle(radius=radius_map_G, degree=5)
+    u3_avg = Average(u3, Lambda, cylinder)
+    v3_avg = Average(v3, Lambda, cylinder)
+
+    
+    dxOmega = Measure("dx", domain=Omega)
+    dxLambda = Measure("dx", domain=Lambda)
+    dsLambda = Measure("ds", domain=Lambda)
+
+    
+    D_area = np.pi * radius_map_G ** 2
+    D_perimeter = 2 * np.pi * radius_map_G
+
+    
+    a00 = perf3 * inner(grad(u3), grad(v3)) * dxOmega + kappa * inner(u3_avg, v3_avg) * D_perimeter * dxLambda
+    a01 = -kappa * inner(u1, v3_avg) * D_perimeter * dxLambda
+    a10 = -kappa * inner(u3_avg, v1) * D_perimeter * dxLambda
+    a11 = perf1 * inner(grad(u1), grad(v1)) * D_area * dxLambda + kappa * inner(u1, v1) * D_perimeter * dxLambda - gamma * inner(u1, v1) * dsLambda
+
+    
+    L0 = inner(Constant(0), v3_avg) * dxLambda
+    L1 = -gamma * inner(P_infty, v1) * dsLambda
+
+    a = [[a00, a01], [a10, a11]]
+    L = [L0, L1]
+
+    W_bcs = [[DirichletBC(V3, del_Omega, boundary_Omega)], []]
+
+    A, b = map(ii_assemble, (a, L))
+    A, b = apply_bc(A, b, W_bcs)
+    A, b = map(ii_convert, (A, b))
+
+    wh = ii_Function(W)
+    solver = LUSolver(A, "mumps")
+    solver.solve(wh.vector(), b)
+    uh3d, uh1d = wh
+    uh3d.rename("3D Pressure", "3D Pressure Distribution")
+    uh1d.rename("1D Pressure", "1D Pressure Distribution")
+
+    
+    
+    s3k = TrialFunction(V3)
+    s1k = TrialFunction(V1)
+    v3k = TestFunction(V3)
+    v1k = TestFunction(V1)
+
+    s3k_avg = Average(s3k, Lambda, cylinder)
+    v3k_avg = Average(v3k, Lambda, cylinder)
+
+    
+    class Uh3dAtLambda(UserExpression):
+        def __init__(self, uh3d, **kwargs):
+            self.uh3d = uh3d
+            super().__init__(**kwargs)
+        def eval(self, value, x):
+            value[0] = self.uh3d(x)
+        def value_shape(self):
+            return ()
+
+    u3h_at_Lambda = interpolate(Uh3dAtLambda(uh3d, degree=uh3d.function_space().ufl_element().degree()), V1)
+
+    
+    a00_sens = perf3 * inner(grad(s3k), grad(v3k)) * dxOmega + kappa * inner(s3k_avg, v3k_avg) * D_perimeter * dxLambda
+    a01_sens = -kappa * inner(s1k, v3k_avg) * D_perimeter * dxLambda
+    a10_sens = -kappa * inner(s3k_avg, v1k) * D_perimeter * dxLambda
+    a11_sens = perf1 * inner(grad(s1k), grad(v1k)) * D_area * dxLambda + kappa * inner(s1k, v1k) * D_perimeter * dxLambda
+
+    
+    L0_sens = inner(uh1d - u3h_at_Lambda, v3k_avg) * D_perimeter * dxLambda
+    L1_sens = inner(u3h_at_Lambda - uh1d, v1k) * D_perimeter * dxLambda
+
+    
+    a_sens = [[a00_sens, a01_sens], [a10_sens, a11_sens]]
+    L_sens = [L0_sens, L1_sens]
+
+    A_sens, b_sens = map(ii_assemble, (a_sens, L_sens))
+    A_sens, b_sens = apply_bc(A_sens, b_sens, W_bcs)
+    A_sens, b_sens = map(ii_convert, (A_sens, b_sens))
+
+    
+    wh_sens = ii_Function(W)
+    solver_sens = LUSolver(A_sens, "mumps")
+    solver_sens.solve(wh_sens.vector(), b_sens)
+    sh3d, sh1d = wh_sens
+    sh3d.rename("Sensitivity 3D", "Sensitivity 3D Distribution")
+    sh1d.rename("Sensitivity 1D", "Sensitivity 1D Distribution")
+
+    
+    os.makedirs(directory_path, exist_ok=True)
+    output_file_1d = os.path.join(directory_path, "pressure1d.vtk")
+    output_file_3d = os.path.join(directory_path, "pressure3d.pvd")
+    output_file_sens_1d = os.path.join(directory_path, "sensitivity1d.vtk")
+    output_file_sens_3d = os.path.join(directory_path, "sensitivity3d.pvd")
+    save_mesh_vtk(Lambda, output_file_1d, radius_map_G, uh1d=uh1d)
+    save_mesh_vtk(Lambda, output_file_sens_1d, radius_map_G, uh1d=sh1d)
+    File(output_file_3d) << uh3d
+    File(output_file_sens_3d) << sh3d
+
+    return output_file_1d, output_file_3d, uh1d, uh3d, sh1d, sh3d, Lambda, Omega
