Refactor changes, attempt to fix flow calculation

modules/FEMSinkVelo.py
@@ -5,12 +5,10 @@ from rtree import index as rtree_index
 import numpy as np
 import os
 
-import MeshCreator
+from MeasureMeshCreator import MeasureMeshCreator, XZeroPlane
 import VTKExporter
 from RadiusFunction import RadiusFunction
-import importlib
 
-importlib.reload(MeshCreator)
 
 class FEMSinkVelo:
     def __init__(
@@ -18,7 +16,7 @@ class FEMSinkVelo:
         G: "FenicsGraph",
         gamma: float,
         gamma_R: float,
-        gamma_v: float,
+        gamma_v: float,   
         gamma_a: float,
         mu: float,
         k_t: float,
@@ -26,68 +24,124 @@ class FEMSinkVelo:
         P_in: float,
         P_cvp: float,
         inlet_points: list[int] = [0],
-        Omega_box: list[float] = None
+        Omega_sink: SubDomain = XZeroPlane()
     ):
-        mesh_data = MeshCreator.create_mesh_and_measures(
-            G,
-            Omega_box=Omega_box,
-            inlet_points=inlet_points,
-        )
-        self.Lambda = mesh_data["Lambda"]
-        self.Omega = mesh_data["Omega"]
+        
+        self.gamma = gamma
+        self.gamma_R = gamma_R
+        self.gamma_a = gamma_a
+        self.P_cvp = P_cvp
+
         self.mu = mu
         self.k_v = k_v
         self.k_t = k_t
 
+        
+        
+        
+        measure_creator = MeasureMeshCreator(G, Omega_sink, Lambda_inlet=inlet_points)
+        mesh_data = measure_creator.create_mesh_and_measures()
+
+        self.Lambda = mesh_data["Lambda"]       
+        self.Omega = mesh_data["Omega"]         
+
+        
         edge_marker = mesh_data["edge_marker"]
         self.dxOmega = mesh_data["dxOmega"]
         self.dxLambda = mesh_data["dxLambda"]
-        self.ds_Omega = mesh_data["dsOmega"]
-        self.ds_Face1 = mesh_data["dsFace1"]
+        self.dsOmega_sink = mesh_data["dsOmegaSink"]       
+        self.dsLambda_robin = mesh_data["dsLambdaNeumann"] 
 
         
-        self.dsLambda_robin = mesh_data["dsLambdaRobin"]  
-        dsLambda_inlet = mesh_data["dsLambdaInlet"]       
-
-        V3 = FunctionSpace(self.Omega, "CG", 1)
-        V1 = FunctionSpace(self.Lambda, "CG", 1)
+        
+        
+        V3 = FunctionSpace(self.Omega, "CG", 1)   
+        V1 = FunctionSpace(self.Lambda, "CG", 1)  
         W = [V3, V1]
+
         u3, u1 = map(TrialFunction, W)
         v3, v1 = map(TestFunction, W)
 
+        
+        
+        
         self.radius_map = RadiusFunction(G, edge_marker, degree=5)
-        cylinder = Circle(radius=self.radius_map, degree=5)
+        cylinder = Circle(radius=self.radius_map, degree=5)  
+
         
         u3_avg = Average(u3, self.Lambda, cylinder)
         v3_avg = Average(v3, self.Lambda, cylinder)
 
+        
         D_area = np.pi * self.radius_map**2
         D_perimeter = 2.0 * np.pi * self.radius_map
 
-        a00 = (Constant(k_t/mu)*inner(grad(u3), grad(v3))*self.dxOmega
-               + Constant(gamma)*u3_avg*v3_avg*D_perimeter*self.dxLambda
-               + Constant(gamma_R)*u3*v3*self.ds_Face1)
         
-        a01 = - Constant(gamma)*u1*v3_avg*D_perimeter*self.dxLambda
-        a10 = - Constant(gamma)*u3_avg*v1*D_perimeter*self.dxLambda
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
-        a11 = (Constant(k_t/mu)*inner(grad(u1), grad(v1))*D_area*self.dxLambda
-               + Constant(gamma)*u1*v1*D_perimeter*self.dxLambda
-               + Constant(gamma_a/mu)*u1*v1*self.dsLambda_robin)
         
+        a00 = (Constant(self.k_t/self.mu)*inner(grad(u3), grad(v3))*self.dxOmega
+               - Constant(self.gamma)*u3_avg*v3_avg*D_perimeter*self.dxLambda  
+               + Constant(self.gamma_R)*u3*v3*self.dsOmega_sink)               
+
+        
+        a01 = (Constant(self.gamma)*u1*v3_avg*D_perimeter*self.dxLambda)
+
+        
+        a10 = (- Constant(self.gamma)*u3_avg*v1*D_perimeter*self.dxLambda)
+
+        
+        a11 = (Constant(self.k_v/self.mu)*inner(grad(u1), grad(v1))*D_area*self.dxLambda
+               + Constant(self.gamma)*u1*v1*D_perimeter*self.dxLambda
+               + Constant(self.gamma_a/self.mu)*u1*v1*self.dsLambda_robin)
+
         a = [[a00, a01],
              [a10, a11]]
 
-        L0 = Constant(gamma_R)*P_cvp*v3*self.ds_Face1
-        L1 = - Constant(gamma_a/mu)*P_cvp*v1*self.dsLambda_robin
+        
+        
+        
+        
+        L0 = Constant(self.gamma_R)*Constant(P_cvp)*v3*self.dsOmega_sink
+        
+        L1 = -Constant(self.gamma_a/self.mu)*Constant(P_cvp)*v1*self.dsLambda_robin
+
         L = [L0, L1]
 
-        inlet_bc = DirichletBC(W[1], Constant(P_in),
-                               mesh_data["lambda_boundary_markers"],
-                               2)
         
-        W_bcs = [[], [inlet_bc]] 
+        
+        
+        
+        inlet_bc = DirichletBC(
+            W[1],
+            Constant(P_in),
+            mesh_data["Lambda_boundary_markers"],
+            2  
+        )
+        W_bcs = [[], [inlet_bc]]
 
+        
+        
+        
         A, b = map(ii_assemble, (a, L))
         A, b = apply_bc(A, b, W_bcs)
         A, b = map(ii_convert, (A, b))
@@ -96,49 +150,50 @@ class FEMSinkVelo:
         solver = LUSolver(A, "mumps")
         solver.solve(wh.vector(), b)
 
+        
         uh3d, uh1d = wh
         uh3d.rename("3D Pressure", "3D Pressure Distribution")
         uh1d.rename("1D Pressure", "1D Pressure Distribution")
+
         self.uh3d, self.uh1d = uh3d, uh1d
 
+        
+        
+        
+        
         V_vector = VectorFunctionSpace(self.Omega, "CG", 1)
         u_velocity = TrialFunction(V_vector)
         v_velocity = TestFunction(V_vector)
-        a_velocity = dot(u_velocity, v_velocity)*dx
-        L_velocity = -Constant(k_t/mu)*dot(grad(self.uh3d), v_velocity)*self.dxOmega
 
-        A_velocity = assemble(a_velocity)
-        b_velocity = assemble(L_velocity)
-        solver_velocity = LUSolver(A_velocity, "mumps")
+        a_vel = dot(u_velocity, v_velocity)*dx
+        L_vel = -Constant(self.k_t/self.mu)*dot(grad(self.uh3d), v_velocity)*self.dxOmega
 
-        velocity = Function(V_vector, name="Velocity Field")
-        solver_velocity.solve(velocity.vector(), b_velocity)
+        A_vel = assemble(a_vel)
+        b_vel = assemble(L_vel)
+        solver_vel = LUSolver(A_vel, "mumps")
+
+        velocity = Function(V_vector, name="Velocity_L2_Projection")
+        solver_vel.solve(velocity.vector(), b_vel)
         velocity.rename("Velocity", "Velocity Field")
         self.velocity = velocity
 
-    def calculate_total_flow_boundary(self) -> float:
+    
+    
+    
+    def calculate_3d_outflow(self) -> float:
+        
+        return assemble(Constant(self.gamma_R)*(self.uh3d - self.P_cvp)*self.dsOmega_sink)
+
+    def calculate_3d_flow_from_velocity(self) -> float:
         
         n = FacetNormal(self.Omega)
-        total_flow = assemble(dot(self.velocity, n)*self.ds_Face1)
-        return f"{total_flow} m^3 / s"
+        return assemble(dot(self.velocity, n)*self.dsOmega_sink)
 
-    def calculate_total_flow_all_boundaries(self) -> float:
-      
-      
-      ds_all = Measure("ds", domain=self.Omega)
-      
-      
-      n = FacetNormal(self.Omega)
-      
-      
-      total_flow = assemble(dot(self.velocity, n) * ds_all)
-      return total_flow
-
-
-    def calculate_total_flow(self) -> float:
+    def calculate_1d_inflow(self) -> float:
         
         mesh1d = self.Lambda
 
+        
         v0 = Vertex(mesh1d, 0)
         edges = list(v0.entities(1))
         if not edges:
@@ -152,23 +207,33 @@ class FEMSinkVelo:
         coords0 = np.array(v0.point().array())
         coords1 = np.array(v1.point().array())
         distance = np.linalg.norm(coords1 - coords0)
+        if distance < 1e-14:
+            return 0.0
 
         p0 = self.uh1d(v0.point())
         p1 = self.uh1d(v1.point())
-        dP_ds = (p1 - p0)/distance if distance > 1e-14 else 0.0
-        print("dP_ds value: " + str(dP_ds))
+        dP_ds = (p1 - p0)/distance
 
-        
         radius0 = float(self.radius_map(v0.point()))
         area0 = np.pi*(radius0**2)
 
-        total_flow = - area0*(self.k_v/self.mu)*dP_ds
-        return total_flow
+        
+        Q_in = - area0*(self.k_v/self.mu)*dP_ds
+        return Q_in
 
+    def calculate_total_flow_all_boundaries(self) -> float:
+        
+        ds_all = Measure("ds", domain=self.Omega)
+        n = FacetNormal(self.Omega)
+        return assemble(dot(self.velocity, n)*ds_all)
+
+    
+    
+    
     def save_vtk(self, directory_path: str):
         
         os.makedirs(directory_path, exist_ok=True)
-        
+
         output_file_1d = os.path.join(directory_path, "pressure1d.vtk")
         output_file_3d = os.path.join(directory_path, "pressure3d.pvd")
         output_file_velocity = os.path.join(directory_path, "velocity3d.pvd")