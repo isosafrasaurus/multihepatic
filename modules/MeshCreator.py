Refactor changes, attempt to fix flow calculation

modules/MeshCreator.py
@@ -1,115 +1,67 @@
-from dolfin import *
+from typing import Optional, Dict, Any, List
+from dolfin import UnitCubeMesh
 from graphnics import FenicsGraph
 from xii import *
 import networkx as nx
 import numpy as np
 
-def create_mesh_and_measures(
-    G: "FenicsGraph",
-    Omega_box: list[float] = None,
-    inlet_points: list[int] = None,
-    # ↑ Renamed from Lambda_endpoints to inlet_points
-):
+def create_mesh(
+    G: FenicsGraph,
+    Omega_bounds_dim: Optional[List[List[float]]] = None,
+    Omega_mesh_voxel_dim: List[int] = [32, 32, 32],
+    Lambda_padding_min: float = 8,
+    Lambda_num_nodes_exp: int = 16,
+) -> Dict[str, Any]:
     """
-    Creates both the 3D mesh (Omega) and the 1D mesh (Lambda) from the graph G.
-    Marks boundary faces/edges in order to impose BCs later.
+    Create meshes for Lambda and Omega domains based on a graph G.
     
-    'inlet_points' is a list of node indices (in G) that should get a Dirichlet BC.
-    All other 1D boundary vertices get a Robin BC.
+    Parameters:
+        G (FenicsGraph): Graph structure used to generate the Lambda mesh.
+        Omega_bounds_dim (Optional[List[List[float]]]): 
+            Bounds for the Omega domain as [[xmin, ymin, zmin], [xmax, ymax, zmax]].
+            If None, the bounding box is determined from node positions with padding.
+        Omega_mesh_voxel_dim (List[int]): Number of voxels in each dimension for the Omega mesh.
+        Lambda_padding_min (float): Padding added around the nodes for Omega bounds when not provided.
+        Lambda_num_nodes_exp (int): Passed to G.make_mesh to define mesh resolution.
+    
+    Returns:
+        Dict[str, Any]: A dictionary containing:
+            - "Lambda": The generated Lambda mesh.
+            - "Omega": The generated and scaled Omega mesh.
+            - "Lambda_edge_marker": Edge markers associated with the Lambda mesh.
     """
-    # 1) Make the 1D mesh from the graph
-    G.make_mesh()
-    Lambda, edge_marker = G.get_mesh()
+    G.make_mesh(n=Lambda_num_nodes_exp)
+    Lambda, Lambda_edge_marker = G.get_mesh()
 
-    # 2) Decide on the 3D bounding box and build the 3D mesh
-    node_positions = nx.get_node_attributes(G, "pos")
-    node_coords = np.asarray(list(node_positions.values()))
-
-    # Just as an example, we pick a UnitCubeMesh and scale/translate it
-    Omega = UnitCubeMesh(32, 32, 32)
+    Omega = UnitCubeMesh(*Omega_mesh_voxel_dim)
     Omega_coords = Omega.coordinates()
 
-    if Omega_box is None:
-        xmax, ymax, zmax = np.max(node_coords, axis=0)
+    node_positions = nx.get_node_attributes(G, "pos")
+    node_coords = np.array(list(node_positions.values()))
+
+    if Omega_bounds_dim is None:
         xmin, ymin, zmin = np.min(node_coords, axis=0)
-        Omega_coords[:, :] *= [xmax - xmin + 10, ymax - ymin + 10, zmax - zmin + 10]
-        Omega_coords[:, :] += [xmin - 5, ymin - 5, zmin - 5]
+        xmax, ymax, zmax = np.max(node_coords, axis=0)
+        scales = np.array([
+            xmax - xmin + 2 * Lambda_padding_min,
+            ymax - ymin + 2 * Lambda_padding_min,
+            zmax - zmin + 2 * Lambda_padding_min
+        ])
+        shifts = np.array([
+            xmin - Lambda_padding_min,
+            ymin - Lambda_padding_min,
+            zmin - Lambda_padding_min
+        ])
     else:
-        Omega_coords[:, :] *= [
-            Omega_box[3] - Omega_box[0],
-            Omega_box[4] - Omega_box[1],
-            Omega_box[5] - Omega_box[2],
-        ]
-        Omega_coords[:, :] += [Omega_box[0], Omega_box[1], Omega_box[2]]
+        lower = np.array(Omega_bounds_dim[0])
+        upper = np.array(Omega_bounds_dim[1])
+        scales = upper - lower
+        shifts = lower
 
-    # 3) Mark the 3D boundary: "Face1"
-    boundary_Omega = MeshFunction("size_t", Omega, Omega.topology().dim() - 1, 0)
-    
-    class Face1(SubDomain):
-        def inside(self, x, on_boundary):
-            return on_boundary and near(x[0], 0.0, DOLFIN_EPS)
-            
-    face1 = Face1()
-    face1.mark(boundary_Omega, 1)
-
-    # 4) Mark the 1D boundary
-    #    All boundary vertices in a 1D mesh are simply the "endpoints" (degree=1 in the graph).
-    #    We want to impose:
-    #       - Dirichlet BC (marker=2) at the inlet_points
-    #       - Robin BC (marker=1) elsewhere on boundary.
-    lambda_boundary_markers = MeshFunction(
-        "size_t", Lambda, Lambda.topology().dim() - 1, 0
-    )
-
-    # First, mark *all* boundary vertices with 1 (Robin)
-    # We can do this by making a subdomain that picks up every boundary vertex
-    class AllBoundary(SubDomain):
-        def inside(self, x, on_boundary):
-            return on_boundary
-
-    all_boundary = AllBoundary()
-    all_boundary.mark(lambda_boundary_markers, 1)
-
-    if inlet_points is not None:
-        # Then override the marker with 2 for each inlet node
-        for node_id in inlet_points:
-            pos = G.nodes[node_id]["pos"]
-
-            class InletEndpoint(SubDomain):
-                def __init__(self, point):
-                    super().__init__()
-                    self.point = point
-
-                def inside(self, x, on_boundary):
-                    return (on_boundary 
-                            and near(x[0], self.point[0], DOLFIN_EPS)
-                            and near(x[1], self.point[1], DOLFIN_EPS)
-                            and near(x[2], self.point[2], DOLFIN_EPS))
-
-            inlet_subdomain = InletEndpoint(pos)
-            inlet_subdomain.mark(lambda_boundary_markers, 2)
-
-    # 5) Create measures
-    dxOmega = Measure("dx", domain=Omega)
-    dxLambda = Measure("dx", domain=Lambda)
-
-    dsOmega = Measure("ds", domain=Omega, subdomain_data=boundary_Omega)
-    dsFace1 = dsOmega(1)
-
-    dsLambda = Measure("ds", domain=Lambda, subdomain_data=lambda_boundary_markers)
-    dsLambda_robin = dsLambda(1)  # For points marked 1 (Robin)
-    dsLambda_inlet = dsLambda(2)  # For points marked 2 (Dirichlet)
+    Omega_coords[:] = Omega_coords * scales + shifts
 
     return {
         "Lambda": Lambda,
         "Omega": Omega,
-        "edge_marker": edge_marker,
-        "boundary_Omega": boundary_Omega,
-        "lambda_boundary_markers": lambda_boundary_markers,
-        "dxOmega": dxOmega,
-        "dxLambda": dxLambda,
-        "dsOmega": dsOmega,
-        "dsFace1": dsFace1,
-        "dsLambdaRobin": dsLambda_robin,
-        "dsLambdaInlet": dsLambda_inlet,
+        "Lambda_edge_marker": Lambda_edge_marker,
     }