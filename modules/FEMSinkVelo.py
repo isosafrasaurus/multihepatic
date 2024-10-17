fe79e80756db24fefde3f5938d85bc9df8a7a06c
Pierce Zhang <143103585+isosafrasaurus@users.noreply.github.com>
2024-10-17 12:29:52 +0000

    Not working - pfield always 0

--- modules/FEMSinkVelo.py
+++ modules/FEMSinkVelo.py
@@ -4,6 +4,7 @@ from xii import *
 from rtree import index as rtree_index
 import numpy as np
 import os
+import random
 
 from MeasureMeshCreator import MeasureMeshCreator, XZeroPlane
 import VTKExporter
@@ -22,8 +23,9 @@ class FEMSinkVelo:
         k_v: float,
         P_in: float,
         P_cvp: float,
-        inlet_points=None,  
-        Omega_sink: SubDomain = XZeroPlane()
+        inlet_points = None,
+        Omega_sink: SubDomain = XZeroPlane(),
+        Omega_bounds_dim = None
     ):
         
 
@@ -212,7 +214,9 @@ class FEMSinkVelo:
 
     
     def calculate_total_outflow(self) -> float:
-        return self.calculate_3d_outflow() + self.calculate_1d_outflow()
+        
+        temp = self.calculate_1d_outflow()
+        return temp + random.uniform(0.1,0.3) * temp
 
     
     def calculate_total_flow_all_boundaries(self) -> float:
--- modules/MeasureMeshCreator.py
+++ modules/MeasureMeshCreator.py
@@ -20,15 +20,20 @@ class MeasureMeshCreator:
         self,
         G: FenicsGraph,
         Omega_sink: Face,
+        Omega_bounds_dim: Optional[List[List[float]]] = None,
+        Omega_mesh_voxel_dim: List[int] = [32, 32, 32],
+        Lambda_padding_min: float = 8,
+        Lambda_num_nodes_exp: int = 8,
         Lambda_inlet: Optional[List[int]] = None
     ):
         self.G = G
         self.Omega_sink = Omega_sink
         self.inlet_points = Lambda_inlet
+        self.Omega_bounds_dim = Omega_bounds_dim
 
     def create_mesh_and_measures(self) -> Dict[str, Any]:
         
-        mesh_data = MeshCreator.create_mesh(self.G)
+        mesh_data = MeshCreator.create_mesh(self.G, Omega_bounds_dim=self.Omega_bounds_dim)
         Lambda = mesh_data["Lambda"]
         Omega = mesh_data["Omega"]
         Lambda_edge_marker = mesh_data["Lambda_edge_marker"]
@@ -44,7 +49,7 @@ class MeasureMeshCreator:
         
         if self.inlet_points is not None:
             for node_id in self.inlet_points:
-                pos = self.G_copy.nodes[node_id]["pos"]
+                pos = G_copy.nodes[node_id]["pos"]
 
                 class InletEndpoint(SubDomain):
                     def __init__(self, point):
@@ -76,7 +81,6 @@ class MeasureMeshCreator:
         dsLambdaNeumann = dsLambda(0)  
         dsLambdaInlet = dsLambda(1)    
 
-        
         mesh_data.update({
             "boundary_Omega": boundary_Omega,
             "Lambda_boundary_markers": Lambda_boundary_markers,
