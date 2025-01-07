771d5bbf4d8533285014560d6cd84c443fd3319a
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2025-01-07 06:06:19 +0000

    Rename meshing

--- modules/meshing/MeasureBuild.py
+++ modules/meshing/MeasureBuild.py
@@ -1,10 +1,9 @@
 import numpy as np
 import importlib
 import os
-
 from dolfin import MeshFunction, SubDomain, Measure, near, DOLFIN_EPS, Mesh
 from graphnics import FenicsGraph
-from .MeshCreator import MeshCreator
+from .MeshBuild import MeshBuild
 from typing import Optional, List
 
 class Face(SubDomain):
@@ -31,7 +30,7 @@ class InletEndpoint(SubDomain):
             and near(x[2], self.point[2])
         )
 
-class MeasureCreator(MeshCreator):
+class MeasureBuild(MeshBuild):
     def __init__(
         self,
         G: FenicsGraph,
@@ -41,21 +40,15 @@ class MeasureCreator(MeshCreator):
     ):
         super().__init__(G, **kwargs)
 
-        boundary_Omega = MeshFunction(
-            "size_t", self.Omega, self.Omega.topology().dim() - 1, 0
-        )
+        boundary_Omega = MeshFunction("size_t", self.Omega, self.Omega.topology().dim() - 1, 0)
         Omega_sink.mark(boundary_Omega, 1)
 
-        boundary_Lambda = MeshFunction(
-            "size_t", self.Lambda, self.Lambda.topology().dim() - 1, 0
-        )
+        boundary_Lambda = MeshFunction("size_t", self.Lambda, self.Lambda.topology().dim() - 1, 0)
         if Lambda_inlet is not None:
             lambda_coordinates = self.Lambda.coordinates()
             for node_id in Lambda_inlet:
                 if not (0 <= node_id < len(lambda_coordinates)):
-                    raise ValueError(
-                        f"Lambda_inlet node_id {node_id} is out of bounds for the Lambda mesh."
-                    )
+                    raise ValueError(f"Lambda_inlet node_id {node_id} is out of bounds for the Lambda mesh.")
                 pos = lambda_coordinates[node_id]
                 inlet_subdomain = InletEndpoint(pos)
                 inlet_subdomain.mark(boundary_Lambda, 1)
