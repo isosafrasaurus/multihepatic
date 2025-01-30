5fe41f0785ee68ad2c4fc9815a179f351ce23d3f
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2025-01-30 15:49:47 +0000

    Add optionality to measure build

--- src/tissue/MeasureBuild.py
+++ src/tissue/MeasureBuild.py
@@ -1,6 +1,7 @@
 import numpy as np
 import importlib
 import os
+import warnings
 from dolfin import MeshFunction, SubDomain, Measure, near, DOLFIN_EPS, Mesh
 from graphnics import FenicsGraph
 from .MeshBuild import MeshBuild
@@ -24,31 +25,38 @@ class MeasureBuild(MeshBuild):
         self,
         G: FenicsGraph,
         Lambda_inlet: Optional[List[int]],
-        Omega_sink: SubDomain,
+        Omega_sink: Optional[SubDomain],
         **kwargs
     ):
         super().__init__(G, **kwargs)
-
         boundary_Omega = MeshFunction("size_t", self.Omega, self.Omega.topology().dim() - 1, 0)
-        Omega_sink.mark(boundary_Omega, 1)
-
         boundary_Lambda = MeshFunction("size_t", self.Lambda, self.Lambda.topology().dim() - 1, 0)
+        
+        if Omega_sink is not None:
+            Omega_sink.mark(boundary_Omega, 1)
+            self.dsOmega = Measure("ds", domain=self.Omega, subdomain_data=boundary_Omega)
+            self.dsOmegaNeumann = self.dsOmega(0)
+            self.dsOmegaSink = self.dsOmega(1)
+        else:
+            warnings.warn("No Lambda inlets provided. Defaulting to Neumann conditions.")
+            self.dsOmega = Measure("ds", domain=self.Omega)
+        
         if Lambda_inlet is not None:
             lambda_coordinates = self.Lambda.coordinates()
             for node_id in Lambda_inlet:
                 if not (0 <= node_id < len(lambda_coordinates)):
                     raise ValueError(f"Lambda_inlet node_id {node_id} is out of bounds for the Lambda mesh.")
-                pos = lambda_coordinates[node_id]
-                inlet_subdomain = InletEndpoint(pos)
+                coordinate = lambda_coordinates[node_id]
+                inlet_subdomain = Point(coordinate)
                 inlet_subdomain.mark(boundary_Lambda, 1)
+            self.dsLambda = Measure("ds", domain=self.Lambda, subdomain_data=boundary_Lambda)
+            self.dsLambdaRobin = self.dsLambda(0)
+            self.dsLambdaInlet = self.dsLambda(1)
+        else:
+            warnings.warn("No Lambda inlets provided.")
+            self.dsLambda = Measure("ds", domain=self.Lambda)
 
         self.dxOmega = Measure("dx", domain=self.Omega)
         self.dxLambda = Measure("dx", domain=self.Lambda)
-        self.dsOmega = Measure("ds", domain=self.Omega, subdomain_data=boundary_Omega)
-        self.dsOmegaNeumann = self.dsOmega(0)
-        self.dsOmegaSink = self.dsOmega(1)
-        self.dsLambda = Measure("ds", domain=self.Lambda, subdomain_data=boundary_Lambda)
-        self.dsLambdaRobin = self.dsLambda(0)
-        self.dsLambdaInlet = self.dsLambda(1)
         self.boundary_Omega = boundary_Omega
         self.boundary_Lambda = boundary_Lambda
