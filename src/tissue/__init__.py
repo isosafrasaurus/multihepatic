79ac8f3f3044672ff2e4e2ec06d291a9a73e0740
Pierce Zhang <pierce.z.zhang@gmail.com>
2025-04-19 22:40:10 -0500

    dependency simplification in sink

--- src/tissue/__init__.py
+++ src/tissue/__init__.py
@@ -1,5 +1,5 @@
 # tissue/__init__.py
 
 from .domain_build import DomainBuild
-from .geometry import AxisPlane, BoundaryPoint
+from .geometry import AxisPlane, BoundaryPoint, AveragingRadius
 from .omega_build import OmegaBuild
