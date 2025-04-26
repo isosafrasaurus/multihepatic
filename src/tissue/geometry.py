6dc299d906f3fd7711f5422667ed680caa96c2e8
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2025-04-26 12:30:11 -0500

    Add SegmentLength measure

--- src/tissue/geometry.py
+++ src/tissue/geometry.py
@@ -20,24 +20,7 @@ class AxisPlane(SubDomain):
         super().__init__()
         self.axis = axis
         self.coordinate = coordinate
-        self.tolerance = tolerance):
+        self.tolerance = tolerance
 
     def inside(self, x, on_boundary):
         return on_boundary and near(x[self.axis], self.coordinate, self.tolerance)
-
-class AveragingRadius(UserExpression):
-    def __init__(self, domain, **kwargs):
-        self.G = domain.fenics_graph
-        self.tree = domain.Lambda.bounding_box_tree()
-        self.tree.build(domain.Lambda)
-        super().__init__(**kwargs)
-
-    def eval(self, value, x):
-        p = Point(x[0], x[1], x[2])
-        cell = self.tree.compute_first_entity_collision(p)
-        if cell == np.iinfo(np.uint32).max:
-            value[0] = 0.0
-        else:
-            edge_ix = self.G.mf[cell]
-            edge = list(self.G.edges())[edge_ix]
-            value[0] = self.G.edges()[edge]['radius']
