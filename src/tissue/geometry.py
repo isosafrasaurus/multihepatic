5a59ffc75ec90770e2956d3f82d8cc675ead2bd9
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2025-05-18 20:10:23 -0700

    Pressure drop plot will now save as csv

--- src/tissue/geometry.py
+++ src/tissue/geometry.py
@@ -8,8 +8,7 @@ class BoundaryPoint(SubDomain):
         self.tolerance = tolerance
 
     def inside(self, x, on_boundary: bool):
-        return (
-            on_boundary
+        return (on_boundary
             and near(x[0], self.coordinate[0], self.tolerance)
             and near(x[1], self.coordinate[1], self.tolerance)
             and near(x[2], self.coordinate[2], self.tolerance)
