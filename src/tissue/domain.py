bc72c6c26b3239d8c34e9e8999f9be4a7362e61a
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2025-05-18 20:10:23 -0700

    Pressure drop plot will now save as csv

--- src/tissue/domain.py
+++ src/tissue/domain.py
@@ -2,42 +2,6 @@ import numpy as np
 from dolfin import SubDomain, MeshFunction, Measure, UnitCubeMesh, facets, near, UserExpression, DOLFIN_EPS, Point
 from .geometry import BoundaryPoint
 
-def get_cells_along_path(G, path, tolerance = DOLFIN_EPS):
-    global_vertices = []
-    global_coords = G.mesh.coordinates()
-    for i in range(len(path) - 1):
-        u, v = path[i], path[i + 1]
-        if G.has_edge(u, v):
-            edge, forward = (u, v), True
-        elif G.has_edge(v, u):
-            edge, forward = (v, u), False
-        else:
-            raise ValueError(f"No edge between {u} and {v} in the graph.")
-        if not "submesh" in G.edges[edge]:
-            raise ValueError("FenicsGraph object submeshes not initialized. Call .make_submeshes()")
-        submesh = G.edges[edge]["submesh"]
-        coords = submesh.coordinates()
-        if hasattr(submesh, 'entity_map'):
-            local_to_global = submesh.entity_map(0)
-        else:
-            local_to_global = []
-            for local_pt in coords:
-                matches = np.where(np.all(np.isclose(global_coords, local_pt, atol = tolerance), axis=1))[0]
-                if len(matches) == 0:
-                    raise ValueError(f"No matching global vertex for local coordinate: {local_pt}")
-                local_to_global.append(matches[0])
-            local_to_global = np.array(local_to_global)
-        tangent = G.edges[edge]["tangent"]
-        if not forward:
-            tangent = -tangent
-        proj = np.dot(coords, tangent)
-        sorted_local_indices = np.argsort(proj)
-        ordered_globals = [local_to_global[idx] for idx in sorted_local_indices]
-        if i > 0 and ordered_globals[0] == global_vertices[-1]:
-            ordered_globals = ordered_globals[1:]
-        global_vertices.extend(ordered_globals)
-    return global_vertices
-
 class AveragingRadius(UserExpression):
     def __init__(self, tree, G, **kwargs):
         super().__init__(**kwargs)
