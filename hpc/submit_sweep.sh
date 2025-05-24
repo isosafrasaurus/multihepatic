5d138106b2dd75cf9e0217f81e16412ae4b11c30
Pierce Zhang <143103585+isosafrasaurus@users.noreply.github.com>
2025-05-24 01:28:49 -0500

    Csv merger

--- hpc/submit_sweep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# submit_sweep.sh
-
-ARRAY_JOB=$(sbatch sweep_array.sbatch | awk '{print $4}')
-sbatch --dependency=afterok:${ARRAY_JOB} --export=ARR_JOB_ID=${ARRAY_JOB} merge_sweep.sbatch
-echo "Submitted array ${ARRAY_JOB} and merge job."
-
