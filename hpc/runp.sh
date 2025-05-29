60b2f6b1427cd0bdee3714c86f08984f0b5b4fee
Pierce Zhang <143103585+isosafrasaurus@users.noreply.github.com>
2025-05-29 16:37:46 -0500

    Set hpc to half mm sweep but probably tooo large/not efficient enough for NOTS

--- hpc/runp.sh
+++ hpc/runp.sh
@@ -11,18 +11,18 @@ export RESULT_ROOT="$HOME/3d-1d/hpc/exp"
 
 # Configurations
 export SWEEP_NAME='gamma'
-export SWEEP_VALUES='np.logspace(-10, 2, 10)'
-export NUM_PARTS=3
-export MAXITER_C=10
-export VOXEL_RES=0.002
+export SWEEP_VALUES='np.logspace(-10, 2, 50)'
+export NUM_PARTS=20
+export MAXITER_C=50
+export VOXEL_RES=0.0005
 export X_DEFAULT='[3.48139681e-03, 2.18907119e-07, 1.13673516e-07]'
 
 # Job settings for SLURM
 export PARTITION='commons'
-export CPUS=4
-export MEM="16G"
-export TIME="00:30:00"
-export JOBNAME="${SWEEP_NAME}_sweep"
+export CPUS=16
+export MEMPERCPU='8G'
+export TIME='24:00:00'
+export JOBNAME="${SWEEP_NAME}_0d5mm"
 
 mkdir -p $LOG_ROOT $RESULT_ROOT
 
@@ -32,7 +32,7 @@ sbatch \
   --nodes=1 \
   --ntasks=1 \
   --cpus-per-task="$CPUS" \
-  --mem="$MEM" \
+  --mem-per-cpu="$MEMPERCPU" \
   --time="$TIME" \
   --array=0-$(($NUM_PARTS-1)) \
   --output="${LOG_ROOT}/%x_%A_%a.out" \
