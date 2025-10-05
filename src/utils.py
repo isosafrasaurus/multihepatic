776b6ce229bc07982a27a09a8894be19c34d32d1
Pierce Zhang <143103585+isosafrasaurus@users.noreply.github.com>
2025-10-05 12:52:28 -0500

    Free field memory helper

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+
+import gc
+from typing import Mapping
+
+def release_result(res: Mapping[str, object]) -> None:
+    if not res: return
+    for k in ("p3d", "p1d", "velocity"):
+        if k in res:
+            res[k] = None  
+    gc.collect()
+
