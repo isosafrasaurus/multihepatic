b277a3291b6f3777519dcf67de9ed8ebccf86e6d
Pierce Zhang <pierce.z.zhang@gmail.com>
2025-10-05 12:53:34 -0500

    fix packages for decoupling

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
-from .core import Domain1D, Domain3D, PhysicsParams, Simulation, SolveResult
+# src/__init__.py
+from .domain import Domain1D, Domain3D
+from .simulation import Simulation
+from .utils import release_result
+
