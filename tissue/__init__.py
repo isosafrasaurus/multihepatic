696ef5077b57d08527b8ab7446dbc0a042b817d0
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2025-10-09 19:33:18 +0000

    Remove xii patches, separated into a different repo

--- tissue/__init__.py
+++ tissue/__init__.py
@@ -1,3 +1,3 @@
-from .domain import AveragingRadius, SegmentLength, get_Omega_rect, get_Omega_rect_from_res
+from .domain import AveragingRadius, SegmentLength, build_mesh_by_counts, build_mesh_by_spacing, build_mesh_by_mm_resolution
 from .geometry import AxisPlane, BoundaryPoint, CubeSubBoundary
 
