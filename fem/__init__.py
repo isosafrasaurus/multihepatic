4ec16c469de9dc38f5f9aaddc116f377423bda03
Pierce Zhang <143103585+isosafrasaurus@users.noreply.github.com>
2025-12-22 12:31:38 -0800

    Moving stuff

--- fem/__init__.py
+++
@@ -1,4 +0,0 @@
-from .operators import ProjectionOperator
-from .solver import BlockLinearSolver
-
-__all__ = ["BlockLinearSolver", "ProjectionOperator"]
--- tissue/__init__.py
+++
@@ -1,3 +0,0 @@
-from .domain import AveragingRadius, SegmentLength, build_mesh_by_counts, build_mesh_by_spacing, \
-    build_mesh_by_mm_resolution
-from .geometry import AxisPlane, BoundaryPoint, CubeSubBoundary
